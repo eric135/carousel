16edc5a40868d7a35589dc5dcb1148830c206d9c
Eric Newberry <enewberry@cs.ucla.edu>
2020-05-27 13:13:37 -0700

    Switch to more robust hashing algorithms

--- bloom.cpp
+++ bloom.cpp
@@ -43,47 +43,146 @@ Bloom::reset()
 size_t
 Bloom::hash1(const std::string& key) const
 {
-  return std::hash<std::string>{}(key) % m_nBits;
+  // Implementation of SDBM hash function derived from http://www.partow.net/programming/hashfunctions/
+  // Copyright 2002 Arash Partow
+  //
+  // Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+  // and associated documentation files (the "Software"), to deal in the Software without
+  // restriction, including without limitation the rights to use, copy, modify, merge, publish,
+  // distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
+  // Software is furnished to do so, subject to the following conditions:
+  //
+  // The above copyright notice and this permission notice shall be included in all copies or
+  // substantial portions of the Software.
+  //
+  // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+  // BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+  // NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+  // DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+  size_t hash = 0;
+  for (size_t i = 0; i < key.size(); i++)
+  {
+    hash = static_cast<uint8_t>(key[i]) + (hash << 6) + (hash << 16) - hash;
+  }
+  return hash % m_nBits;
 }
 
 size_t
 Bloom::hash2(const std::string& key) const
 {
-  size_t sum = 0;
-  for (char c : key) {
-    sum += static_cast<size_t>(c);
+  // Implementation of DJP hash function derived from http://www.partow.net/programming/hashfunctions/
+  // Copyright 2002 Arash Partow
+  //
+  // Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+  // and associated documentation files (the "Software"), to deal in the Software without
+  // restriction, including without limitation the rights to use, copy, modify, merge, publish,
+  // distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
+  // Software is furnished to do so, subject to the following conditions:
+  //
+  // The above copyright notice and this permission notice shall be included in all copies or
+  // substantial portions of the Software.
+  //
+  // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+  // BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+  // NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+  // DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+  size_t hash = 5381;
+  for (size_t i = 0; i < key.size(); i++) {
+    hash = ((hash << 5) + hash) + static_cast<uint8_t>(key[i]);
   }
-  return sum % m_nBits;
+  return hash % m_nBits;
 }
 
 size_t
 Bloom::hash3(const std::string& key) const
 {
-  size_t sum = 0;
-  for (size_t i = 0; i < key.size(); i += 2) {
-    sum += static_cast<size_t>(key[i]);
+  // Implementation of DEK hash function derived from http://www.partow.net/programming/hashfunctions/
+  // Copyright 2002 Arash Partow
+  //
+  // Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+  // and associated documentation files (the "Software"), to deal in the Software without
+  // restriction, including without limitation the rights to use, copy, modify, merge, publish,
+  // distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
+  // Software is furnished to do so, subject to the following conditions:
+  //
+  // The above copyright notice and this permission notice shall be included in all copies or
+  // substantial portions of the Software.
+  //
+  // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+  // BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+  // NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+  // DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+  size_t hash = key.size();
+  for (size_t i = 0; i < key.size(); i++) {
+    hash = ((hash << 5) ^ (hash >> 27)) ^ static_cast<uint8_t>(key[i]);
   }
-  return sum % m_nBits;
+  return hash % m_nBits;
 }
 
 size_t
 Bloom::hash4(const std::string& key) const
 {
-  size_t sum = 0;
-  for (size_t i = 1; i < key.size(); i += 2) {
-    sum += static_cast<size_t>(key[i]);
+  // Implementation of JS hash function derived from http://www.partow.net/programming/hashfunctions/
+  // Copyright 2002 Arash Partow
+  //
+  // Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+  // and associated documentation files (the "Software"), to deal in the Software without
+  // restriction, including without limitation the rights to use, copy, modify, merge, publish,
+  // distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
+  // Software is furnished to do so, subject to the following conditions:
+  //
+  // The above copyright notice and this permission notice shall be included in all copies or
+  // substantial portions of the Software.
+  //
+  // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+  // BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+  // NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+  // DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+  size_t hash = 1315423911;
+  for (size_t i = 0; i < key.size(); i++) {
+    hash ^= ((hash << 5) + static_cast<uint8_t>(key[i]) + (hash >> 2));
   }
-  return sum % m_nBits;
+  return hash % m_nBits;
 }
 
 size_t
 Bloom::hash5(const std::string& key) const
 {
-  size_t sum = 0;
-  for (size_t i = key.size() / 2; i < key.size(); i++) {
-    sum += static_cast<size_t>(key[i]);
+  // Implementation of PJW hash function derived from http://www.partow.net/programming/hashfunctions/
+  // Copyright 2002 Arash Partow
+  //
+  // Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+  // and associated documentation files (the "Software"), to deal in the Software without
+  // restriction, including without limitation the rights to use, copy, modify, merge, publish,
+  // distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
+  // Software is furnished to do so, subject to the following conditions:
+  //
+  // The above copyright notice and this permission notice shall be included in all copies or
+  // substantial portions of the Software.
+  //
+  // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+  // BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+  // NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+  // DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+  const size_t BitsInUnsignedInt = sizeof(size_t) * 8;
+  const size_t ThreeQuarters = (BitsInUnsignedInt  * 3) / 4;
+  const size_t OneEighth = BitsInUnsignedInt / 8;
+  const size_t HighBits = static_cast<size_t>(0xFFFFFFFF) << (BitsInUnsignedInt - OneEighth);
+
+  size_t hash = 0;
+  size_t test = 0;
+  for (size_t i = 0; i < key.size(); i++) {
+    hash = (hash << OneEighth) + static_cast<uint8_t>(key[i]);
+    if ((test = hash & HighBits) != 0) {
+      hash = ((hash ^ (test >> ThreeQuarters)) & (~HighBits));
+    }
   }
-  return sum % m_nBits;
+  return hash;
 }
 
 } // namespace carousel
