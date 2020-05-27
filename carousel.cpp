6c5cd71d2bc2714580dfb93cecf3f888948f7e76
Eric Newberry <enewberry@cs.ucla.edu>
2020-05-26 23:49:09 -0700

    Implement Carousel algorithm

--- carousel.cpp
+++ carousel.cpp
@@ -3,23 +3,88 @@
 
 #include "carousel.hpp"
 
+#include <cmath>
+
 namespace carousel {
 
-Carousel::Carousel(const LogCallback& callback)
+Carousel::Carousel(const LogCallback& callback,
+                   size_t memorySize,
+                   size_t collectionTime,
+                   size_t highThreshold,
+                   size_t bloomFilterSize)
   : m_callback(callback)
+  , m_bloom(bloomFilterSize)
+  , m_memorySize(memorySize)
+  , m_collectionTime(collectionTime)
+  , m_highThreshold(highThreshold)
 {
+  // TODO: Is this the correct way to determine the phase duration?
+  m_phaseDuration = static_cast<double>(collectionTime) / static_cast<double>(memorySize);
 }
 
 void
 Carousel::log(const std::string& key, const std::string& entry)
 {
-  // TODO
+  size_t hash = std::hash<std::string>{}(key);
+
+  // Check if entry matches the current phase
+  if ((hash & m_kMask) == m_currentPhase) {
+
+    // Check if likely (bloom filter) already stored this key this phase
+    if (m_bloom.isEvidenced(key)) {
+      // Skip since likely already logged this phase
+      return;
+    }
+
+    m_bloom.add(key);
+    m_nMatchingThisPhase++;
+
+    // Call callback to log this entry
+    m_callback(key, entry);
+
+    if (m_nMatchingThisPhase > m_highThreshold) {
+      // We've exceeded the number that can match during this phase => repartition
+      repartition();
+      return;
+    }
+  }
+
+  m_nThisPhase++;
+
+  if (m_nThisPhase >= m_phaseDuration) {
+    // Time to go to the next phase
+    startNextPhase();
+  }
 }
 
 void
 Carousel::reset()
 {
-  // TODO
+  m_bloom.reset();
+  m_k = 1;
+  m_kMask = 1;
+  m_currentPhase = 0;
+  m_nThisPhase = 0;
+  m_nMatchingThisPhase = 0;
+}
+
+void
+Carousel::startNextPhase()
+{
+  m_bloom.reset();
+  m_currentPhase = (m_currentPhase + 1) % static_cast<size_t>(std::pow(2, m_k - 1));
+  m_nThisPhase = 0;
+  m_nMatchingThisPhase = 0;
+}
+
+void
+Carousel::repartition()
+{
+  m_k++;
+  m_kMask += std::pow(2, m_k - 1);
+  m_currentPhase = 0;
+  m_nThisPhase = 0;
+  m_nMatchingThisPhase = 0;
 }
 
 } // namespace carousel
--- carousel.hpp
+++ carousel.hpp
@@ -4,6 +4,8 @@
 #ifndef CAROUSEL_CAROUSEL_HPP
 #define CAROUSEL_CAROUSEL_HPP
 
+#include "bloom.hpp"
+
 #include <functional>
 #include <optional>
 #include <string>
@@ -13,30 +15,59 @@ namespace carousel {
 class Carousel
 {
 public:
-  typedef std::function<void(const std::string&)> LogCallback;
+  typedef std::function<void(const std::string&, const std::string&)> LogCallback;
 
 public:
   /**
    * \brief Creates an instance of Carousel that outputs to the specified callback
+   * \param memorySize Number of sources that can be logged
+   * \param collectionTime Estimated number of total entries to be processed
+   * \param highThreshold Upper bound on number of matching keys within a phase before repartitioning
+   * \param bloomFilterBits Number of bits to use in bloom filter. Greater values can increase accuracy
    */
-  Carousel(const LogCallback& callback);
+  Carousel(const LogCallback& callback,
+           size_t memorySize,
+           size_t collectionTime,
+           size_t highThreshold = 32,
+           size_t bloomFilterBits = 128);
 
   /**
    * \brief Submit the specified entry to Carousel
    * \param key Logging key
    * \param entry Entry for log for the given key
+   *
+   * Whether Carousel logs this entry depends upon whether it falls into the current phase
    */
   void
   log(const std::string& key, const std::string& entry);
 
   /**
-   * \brief Resets Carousel
+   * \brief Reset Carousel
    */
   void
   reset();
 
 private:
+  void
+  startNextPhase();
+
+  void
+  repartition();
+
+private:
   LogCallback m_callback;
+  Bloom m_bloom;
+
+  size_t m_memorySize;
+  size_t m_collectionTime;
+  size_t m_highThreshold;
+  size_t m_phaseDuration = 0;
+
+  size_t m_k = 1;
+  size_t m_kMask = 1;
+  size_t m_currentPhase = 0;
+  size_t m_nThisPhase = 0;
+  size_t m_nMatchingThisPhase = 0;
 };
 
 } // namespace carousel
