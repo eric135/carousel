2970bcf14242c458ba56db9693f94fe600258636
Boyan Ding <boyan.j.ding@gmail.com>
2020-06-05 16:17:41 -0700

    Implement logger used for simulation

---
+++ simlogger.cpp
@@ -0,0 +1,61 @@
+#include <functional>
+
+#include "simlogger.hpp"
+
+using std::placeholders::_1;
+using std::placeholders::_2;
+
+SimLogger::SimLogger(const carousel::SimulationTimer& timer,
+                     size_t memorySize,
+                     uint64_t collectionInterval)
+  : m_timer(timer)
+  , m_memorySize(memorySize)
+  , m_interval(collectionInterval)
+  , m_lastLog(0)
+{
+}
+
+void
+SimLogger::logCallback(const std::string& key, const std::string& content)
+{
+  if (m_logging_queue.size() < m_memorySize) {
+    m_logging_queue.push_back(key);
+  }
+  uint64_t now = m_timer.now();
+  if (now - m_lastLog >= m_interval) {
+    m_db.insert(m_logging_queue.front());
+    m_logging_queue.pop_front();
+    m_lastLog = now;
+  }
+}
+
+NaiveLogger::NaiveLogger(const carousel::SimulationTimer& timer,
+                         size_t memorySize,
+                         uint64_t collectionInterval)
+  : SimLogger(timer, memorySize, collectionInterval)
+{
+}
+
+void
+NaiveLogger::inputData(const std::string& key, const std::string& content)
+{
+  logCallback(key, content);
+}
+
+CarouselLogger::CarouselLogger(const carousel::SimulationTimer& timer,
+                               size_t memorySize,
+                               uint64_t collectionInterval)
+  : SimLogger(timer, memorySize, collectionInterval)
+  , m_carousel(bind(&CarouselLogger::logCallback, this, _1, _2),
+               memorySize,
+               timer,
+               collectionInterval)
+{
+  m_carousel.reset();
+}
+
+void
+CarouselLogger::inputData(const std::string& key, const std::string& content)
+{
+  m_carousel.log(key, content);
+}
---
+++ simlogger.hpp
@@ -0,0 +1,66 @@
+#ifndef CAROUSEL_SIMLOGGER_HPP
+#define CAROUSEL_SIMLOGGER_HPP
+
+#include <deque>
+#include <unordered_set>
+
+#include "carousel.hpp"
+#include "simulation-timer.hpp"
+
+extern template class carousel::Carousel<uint64_t,uint64_t>;
+typedef carousel::Carousel<uint64_t,uint64_t> SimCarousel;
+
+class SimLogger
+{
+public:
+  SimLogger(const carousel::SimulationTimer& timer,
+            size_t memorySize,
+            uint64_t collectionInterval);
+
+  void
+  logCallback(const std::string& key, const std::string& content);
+
+  virtual void
+  inputData(const std::string& key, const std::string& content) = 0;
+
+  inline size_t
+  recorded_keys() const
+  {
+    return m_db.size();
+  }
+
+private:
+  const carousel::SimulationTimer& m_timer;
+  std::deque<std::string> m_logging_queue;
+  std::unordered_set<std::string> m_db;
+  uint64_t m_interval;
+  uint64_t m_lastLog;
+  size_t m_memorySize;
+};
+
+class NaiveLogger : public SimLogger
+{
+public:
+  NaiveLogger(const carousel::SimulationTimer& timer,
+              size_t memorySize,
+              uint64_t collectionInterval);
+
+  void
+  inputData(const std::string& key, const std::string& content);
+};
+
+class CarouselLogger : public SimLogger
+{
+public:
+  CarouselLogger(const carousel::SimulationTimer& timer,
+                 size_t memorySize,
+                 uint64_t collectionInterval);
+
+  void
+  inputData(const std::string& key, const std::string& content);
+
+private:
+  SimCarousel m_carousel;
+};
+
+#endif // CAROUSEL_SIMLOGGER_HPP
